0cdd6ec0d4d46357fa0b461924a5f44bf58c56cd
lemur9 <yijiesanren@mail.com>
2024-11-25 20:17:22 +0800

    open social media apps in the default tag page.

--- config.h
+++ config.h
@@ -43,6 +43,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "QQ",       NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "wechat",   NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
